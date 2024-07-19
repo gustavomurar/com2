fbf2989d25c8c3cdb90926795e01d63e2009f053
gustavomurar <gustavomurarosoares3@gmail.com>
2024-07-19 15:04:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import ProductList from '@/components/ProductList.vue'
+
+const produtos = ref ([
+  {id: 1, nome: 'Produto A' , preco: 'R$110,15'},
+  {id: 2, nome: 'Produto B' , preco: 'R$210,55'},
+  {id: 3, nome: 'Produto C' , preco: 'R$120,37'},
+  {id: 4, nome: 'Produto D' , preco: 'R$340,99'}
+
+])
+
+function excluirProduto(id) {
+  const index = produtos.value.findIndex((produto) => produto.id === id)
+  produtos.value.splice(index, 1)
+}
+
+function excluirTodosOsProdutos() {
+  produtos.value = []
+}
+</script>
+
+<template>
+  <h1>Meus produtos</h1>
+  <product-list :produtos="produtos" @excluir="excluirProduto" @excluirTudo="excluirTodosOsProdutos"/>
+</template>
+
+<style scoped></style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps(['produtos'])
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Preço</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="produto in produtos" :key="produto.id">
+                <td>{{ produto.id }}</td>
+                <td>{{ produto.nome }}</td>
+                <td>{{ produto.preco }}</td>
+                <td><i class="mdi mdi-delete" @click="$emit('excluir', produto.id)"></i></td>
+            </tr>
+        </tbody>
+    </table>
+    <button @click="$emit('excluirTudo')">Excluir Tudo</button>
+</template>
